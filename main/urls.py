1a2c0ba4da9339bea386605f3499f2db581fb89b
salohiddin674 <salohiddinzokirjonov819@gmail.com>
2024-04-08 14:51:14 +0500

    salohiddin

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,16 @@
 from django.urls import path
+from .views import *
 
 urlpatterns = [
+    path('',index_view, name='index_url'),
+    path('shop',shop_view, name='shop_url'),
+    path('about', about_view, name='about_url'),
+    path('blog',blog_view, name='blog_url'),
+    path('contact', contact_view, name='contact_url'),
+    path('cart', cart_view, name='cart_url'),
+    path('login', login_view, name='login_url'),
+    path('my-account', my_account, name='my_account_url'),
+    path('wishlist', wishlist_view, name='wishlist_url'),
+
 
 ]
--- main/views.py
+++ main/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index_view(request):
+    return render(request,'index.html')
+
+def shop_view(request):
+    return render(request,'shop.html')
+
+def about_view(request):
+    return render(request,'about.html')
+
+def blog_view(request):
+    return render(request,'blog.html')
+
+def contact_view(request):
+    return render(request,'contact.html')
+
+def cart_view(request):
+    return render(request,'cart.html')
+
+def login_view(request):
+    return render(request,'login.html')
+
+def my_account(request):
+    return render(request,'my-account.html')
+
+def wishlist_view(request):
+    return render(request,'wishlist.html')
